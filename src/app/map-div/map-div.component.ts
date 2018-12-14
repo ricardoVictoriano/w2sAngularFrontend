59306c5c7dcf8ce16aa946f24dfccd58b249a962
José Victoriano <jose.victoriano@wakaru.eu>
2018-12-14 18:21:37 +0000

    Back to newBranch

---
+++ src/app/map-div/map-div.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-div',
+  templateUrl: './map-div.component.html',
+  styleUrls: ['./map-div.component.css']
+})
+export class MapDivComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
